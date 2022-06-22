2c15aece75e4bb9e7bcf3cf810ff18cff292b9c8
Conrad <59329280+conrad-krueger@users.noreply.github.com>
2022-06-22 18:07:29 -0400

    Add files via upload

---
+++ Games/taylor swift/conwayv4/conwayv4App.swift
@@ -0,0 +1,17 @@
+//
+//  conwayv4App.swift
+//  conwayv4
+//
+//  Created by Conrad Krueger on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct conwayv4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
